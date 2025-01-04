7d2693ef81084978043563b1f0dfa8feb3e1d904
yousefalotaibi24 <yousefalotaibi21@gmail.com>
2025-01-04 15:34:36 +0300

    ..

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -3,8 +3,18 @@ import Nav from './Nav'
 const Home = () => {
   return (
     <div>
-      <Nav/>
-      Home
+      <div><Nav/></div>
+      <div className='card'>
+      <div className='card'>1</div>
+      <div className='card'>2</div>
+      <div className='card'>3</div>
+      <div className='card'>4</div>
+      <div className='card'>5</div>
+      <div className='card'>6</div>
+     
+      </div>
+      
+      
     </div>
   )
 }
